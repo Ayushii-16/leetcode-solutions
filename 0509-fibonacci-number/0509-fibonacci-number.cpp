a198fc2299c93b7b64a0300c511846f2057662d3
Ayushi <ayushimaravi375@gmail.com>
2026-07-24 21:31:04 +0530

    Time: 3 ms (65.19%), Space: 8.2 MB (11.24%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int solve(int n,vector<int> &dp){
+
+        if(n == 1 || n == 2) return 1;
+        if(dp[n] != -1)  return dp[n];
+       return dp[n] = solve(n-2,dp) +solve(n-1,dp);
+
+    }
+
+    int fib(int n) {
+        if(n == 0) return 0;
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+
+    }
+};
